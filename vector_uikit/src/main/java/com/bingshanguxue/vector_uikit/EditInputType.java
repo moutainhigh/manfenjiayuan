e75b0c0b8632ec25899a4deec1b53fa2c39a5419
bingshanguxue <bingshanguxue@gmail.com>
2016-08-04 09:52:11 +0800

    optimize 'uikit' module

---
+++ vector_uikit/src/main/java/com/bingshanguxue/vector_uikit/EditInputType.java
@@ -0,0 +1,13 @@
+package com.bingshanguxue.vector_uikit;
+
+/**
+ *
+ * Created by bingshanguxue on 8/3/16.
+ */
+public interface EditInputType {
+    int TEXT = 0;//text
+    int NUMBER_DECIMAL = 1;//numberDecimal
+    int BARCODE = 2;//数字
+    int PRICE = 3;//小数，且最多保留两位有效数字
+
+}
