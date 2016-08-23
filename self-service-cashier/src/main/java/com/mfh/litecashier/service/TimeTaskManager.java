3109cdce684b8417422d5e03cdfbb665371404cf
bingshanguxue <bingshanguxue@gmail.com>
2016-08-23 17:32:22 +0800

    新增 定时同步商品功能

--- self-service-cashier/src/main/java/com/mfh/litecashier/service/TimeTaskManager.java
+++ self-service-cashier/src/main/java/com/mfh/litecashier/service/TimeTaskManager.java
@@ -4,6 +4,7 @@ import android.os.Handler;
 import android.os.Message;
 
 import com.mfh.framework.BizConfig;
+import com.mfh.framework.core.logger.ZLogger;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,6 +16,10 @@ public class TimeTaskManager {
     private static final int SECOND = 1000;
     private static final int MINUTE = 60 * 1000;
     private static final int HOUR = 60 * 60 * 1000;
+
+    private static final int MSG_WHAT_SYNC_POSORDER = 1;
+    private static final int MSG_WHAT_SYNC_POSGOODS = 2;
+
 
     private static TimeTaskManager instance = null;
 
@@ -35,20 +40,47 @@ public class TimeTaskManager {
     }
 
 
+    /**
+     * 同步订单
+     * */
     private final Timer syncPosOrderTimer = new Timer();
     private TimerTask syncPosOrderTask = new TimerTask() {
         @Override
         public void run() {
             Message message = new Message();
-            message.what = 1;
+            message.what = MSG_WHAT_SYNC_POSORDER;
             handler.sendMessage(message);
         }
     };
+
+    /**
+     * 同步商品
+     * */
+    private final Timer syncGoodsTimer = new Timer();
+    private TimerTask syncGoodsTask = new TimerTask() {
+        @Override
+        public void run() {
+            Message message = new Message();
+            message.what = MSG_WHAT_SYNC_POSGOODS;
+            handler.sendMessage(message);
+        }
+    };
+
+
     static Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             // TODO Auto-generated method stub
-            UploadSyncManager.getInstance().sync(UploadSyncManager.SyncStep.CASHIER_ORDER);
+            switch (msg.what){
+                case MSG_WHAT_SYNC_POSORDER:{
+                    UploadSyncManager.getInstance().sync(UploadSyncManager.SyncStep.CASHIER_ORDER);
+                }
+                break;
+                case MSG_WHAT_SYNC_POSGOODS:{
+                    DataSyncManager.get().sync(DataSyncManager.SYNC_STEP_PRODUCTS);
+                }
+                break;
+            }
 
             // 要做的事情
             super.handleMessage(msg);
@@ -57,15 +89,19 @@ public class TimeTaskManager {
 
     /**定时同步POS订单*/
     public void start(){
+        ZLogger.d("定时任务开启...");
         if (BizConfig.RELEASE){
-            syncPosOrderTimer.schedule(syncPosOrderTask, 10 * MINUTE, 10 * 1000);
+            syncPosOrderTimer.schedule(syncPosOrderTask, 10 * SECOND, 10 * MINUTE);
+            syncGoodsTimer.schedule(syncGoodsTask, 10 * SECOND, 6 * HOUR);
         }
         else{
-            syncPosOrderTimer.schedule(syncPosOrderTask, 10 * SECOND, 10 * 1000);
+            syncPosOrderTimer.schedule(syncPosOrderTask, 10 * SECOND, 10 * SECOND);
+            syncGoodsTimer.schedule(syncGoodsTask, 10 * SECOND, 1 * MINUTE);
         }
     }
 
     public void cancel(){
+        ZLogger.d("取消定时任务...");
         syncPosOrderTimer.cancel();
     }
 }
