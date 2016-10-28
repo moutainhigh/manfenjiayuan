8cebb28a42bc42a49ccb34b0c7c820bcc8aa372c
bingshanguxue <bingshanguxue@gmail.com>
2016-10-28 12:06:32 +0800

    新增 买手功能

---
+++ pda-supermarket-idata2/src/main/java/com/manfenjiayuan/pda_supermarket/ui/buyer/AcceptPrepareOrdersFragment.java
@@ -0,0 +1,362 @@
+package com.manfenjiayuan.pda_supermarket.ui.buyer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bingshanguxue.pda.widget.ScanBar;
+import com.bingshanguxue.vector_uikit.slideTab.TopFragmentPagerAdapter;
+import com.bingshanguxue.vector_uikit.slideTab.TopSlidingTabStrip;
+import com.manfenjiayuan.business.presenter.ScOrderPresenter;
+import com.manfenjiayuan.business.view.IScOrderView;
+import com.manfenjiayuan.pda_supermarket.R;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderBarcodeFragment;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderEvent;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderInfoFragment;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderItemsFragment;
+import com.mfh.comn.bean.PageInfo;
+import com.mfh.comn.net.data.IResponseData;
+import com.mfh.comn.net.data.RspValue;
+import com.mfh.framework.MfhApplication;
+import com.mfh.framework.anlaysis.logger.ZLogger;
+import com.mfh.framework.api.scOrder.ScOrder;
+import com.mfh.framework.api.scOrder.ScOrderApiImpl;
+import com.mfh.framework.core.utils.DialogUtil;
+import com.mfh.framework.core.utils.NetworkUtils;
+import com.mfh.framework.core.utils.StringUtils;
+import com.mfh.framework.network.NetCallBack;
+import com.mfh.framework.network.NetProcessor;
+import com.mfh.framework.uikit.base.BaseFragment;
+import com.mfh.framework.uikit.dialog.ProgressDialog;
+import com.mfh.framework.uikit.widget.ViewPageInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
+
+
+/**
+ * 新建拣货单：线上订单抢单
+ * Created by Nat.ZZN(bingshanguxue) on 15/8/30.
+ */
+public class AcceptPrepareOrdersFragment extends BaseFragment implements IScOrderView {
+
+    public static final String EXTRA_KEY_SCORDER = "scOrder";
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.scanBar)
+    public ScanBar mScanBar;
+    @Bind(R.id.tab_page)
+    TopSlidingTabStrip mTabStrip;
+    @Bind(R.id.viewpager_pagecontent)
+    ViewPager mViewPager;
+    private TopFragmentPagerAdapter viewPagerAdapter;
+
+    @Bind(R.id.fab_submit)
+    public FloatingActionButton btnSubmit;
+
+
+    private ScOrder mScOrder;
+    private ScOrderPresenter mScOrderPresenter;
+
+    public static AcceptPrepareOrdersFragment newInstance(Bundle args) {
+        AcceptPrepareOrdersFragment fragment = new AcceptPrepareOrdersFragment();
+
+        if (args != null) {
+            fragment.setArguments(args);
+        }
+        return fragment;
+    }
+
+    @Override
+    protected boolean isResponseBackPressed() {
+        return true;
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_invfind_create;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        //清空签收数据库
+//        InvRecvGoodsService.get().clear();
+
+        mScOrderPresenter = new ScOrderPresenter(this);
+    }
+
+    @Override
+    protected void createViewInner(View rootView, ViewGroup container, Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        if (args != null) {
+            animType = args.getInt(EXTRA_KEY_ANIM_TYPE, ANIM_TYPE_NEW_NONE);
+            mScOrder = (ScOrder) args.getSerializable(EXTRA_KEY_SCORDER);
+        }
+
+        mToolbar.setTitle("抢单");
+        if (animType == ANIM_TYPE_NEW_FLOW) {
+            mToolbar.setNavigationIcon(R.drawable.ic_toolbar_close);
+        } else {
+            mToolbar.setNavigationIcon(R.drawable.ic_toolbar_back);
+        }
+        mToolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getActivity().onBackPressed();
+                    }
+                });
+//        // Set an OnMenuItemClickListener to handle menu item clicks
+//        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                // Handle the menu item
+//                int id = item.getItemId();
+//                if (id == R.id.action_submit) {
+//                    submit();
+//                }
+//                return true;
+//            }
+//        });
+//        // Inflate a menu to be displayed in the toolbar
+//        mToolbar.inflateMenu(R.menu.menu_inv_recv);
+        mScanBar.setVisibility(View.GONE);
+
+        initTabs();
+
+        reload();
+    }
+
+    @Override
+    public boolean onBackPressed() {
+//        if (goodsAdapter.getItemCount() > 0) {
+//            showConfirmDialog("退出后商品列表将会清空，确定要退出吗？",
+//                    "退出", new DialogInterface.OnClickListener() {
+//
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.dismiss();
+//
+//                            getActivity().setResult(Activity.RESULT_CANCELED);
+//                            getActivity().finish();
+//                        }
+//                    }, "点错了", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.dismiss();
+//                        }
+//                    });
+//        } else {
+        getActivity().setResult(Activity.RESULT_CANCELED);
+        getActivity().finish();
+//        }
+
+        return isResponseBackPressed();
+    }
+
+
+    private void initTabs() {
+        mTabStrip.setOnClickTabListener(null);
+        mTabStrip.setOnPagerChange(new TopSlidingTabStrip.OnPagerChangeLis() {
+            @Override
+            public void onChanged(int page) {
+//                refreshData(page);
+//                if (page != 0){
+//                    mScanBar.requestFocusEnd();
+//                }
+            }
+        });
+
+        viewPagerAdapter = new TopFragmentPagerAdapter(getChildFragmentManager(),
+                mTabStrip, mViewPager, com.bingshanguxue.pda.R.layout.tabitem_text_large);
+        ArrayList<ViewPageInfo> mTabs = new ArrayList<>();
+        mTabs.add(new ViewPageInfo("订单", "订单", ScOrderInfoFragment.class,
+                null));
+        mTabs.add(new ViewPageInfo("条码", "条码", ScOrderBarcodeFragment.class,
+                null));
+        mTabs.add(new ViewPageInfo("商品", "商品", ScOrderItemsFragment.class,
+                null));
+        viewPagerAdapter.addAllTab(mTabs);
+        mViewPager.setOffscreenPageLimit(mTabs.size());
+    }
+
+    /**
+     * 通知更新
+     */
+    private void notifyScOrder(boolean isDelay) {
+        if (isDelay) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Bundle args = new Bundle();
+                    args.putSerializable(ScOrderEvent.EXTRA_KEY_SCORDER, mScOrder);
+                    EventBus.getDefault().post(new ScOrderEvent(ScOrderEvent.EVENT_ID_UPDATE, args));
+                }
+            }, 1000);
+        } else {
+            Bundle args = new Bundle();
+            args.putSerializable(ScOrderEvent.EXTRA_KEY_SCORDER, mScOrder);
+            EventBus.getDefault().post(new ScOrderEvent(ScOrderEvent.EVENT_ID_UPDATE, args));
+        }
+    }
+
+
+    /**
+     * 刷新
+     */
+    private void reload() {
+        if (mScOrder == null) {
+            DialogUtil.showHint("订单无效");
+            getActivity().setResult(Activity.RESULT_CANCELED);
+            getActivity().finish();
+        } else {
+            if (!NetworkUtils.isConnect(MfhApplication.getAppContext())) {
+                DialogUtil.showHint(R.string.toast_network_error);
+                refresh(null);
+                return;
+            }
+
+            mScOrderPresenter.getByBarcode(mScOrder.getBarcode(), mScOrder.getStatus(), true);
+        }
+    }
+
+    private void refresh(ScOrder scOrder) {
+        mScOrder = scOrder;
+        notifyScOrder(true);
+    }
+
+    @Override
+    public void onIScOrderViewProcess() {
+
+    }
+
+    @Override
+    public void onIScOrderViewError(String errorMsg) {
+        refresh(null);
+    }
+
+    @Override
+    public void onIScOrderViewSuccess(PageInfo pageInfo, List<ScOrder> dataList) {
+    }
+
+    @Override
+    public void onIScOrderViewSuccess(ScOrder data) {
+        refresh(data);
+    }
+
+
+//    /**
+//     * 拣货-抢单
+//     */
+//    @OnClick(R.id.fab_submit)
+//    public void submit() {
+//        btnSubmit.setEnabled(false);
+//        showConfirmDialog("确定要抢单吗？",
+//                "确定", new DialogInterface.OnClickListener() {
+//
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//
+//
+//                    }
+//                }, "点错了", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//
+//                        btnSubmit.setEnabled(true);
+//                    }
+//                });
+//    }
+
+    /**
+     * 抢单
+     */
+    @OnClick(R.id.fab_submit)
+    public void submitStep1() {
+        showProgressDialog(ProgressDialog.STATUS_PROCESSING, "抢单中...", false);
+
+        if (!NetworkUtils.isConnect(MfhApplication.getAppContext())) {
+            onSubmitError(getString(R.string.toast_network_error));
+            return;
+        }
+
+
+        ScOrderApiImpl.acceptOrderWhenOrdered(mScOrder.getId(), responseRC);
+    }
+
+    NetCallBack.NetTaskCallBack responseRC = new NetCallBack.NetTaskCallBack<String,
+            NetProcessor.Processor<String>>(
+            new NetProcessor.Processor<String>() {
+                @Override
+                public void processResult(IResponseData rspData) {
+//                    {"code":"0","msg":"更新成功!","version":"1","data":null}
+                    //java.lang.ClassCastException: com.mfh.comn.net.data.RspValue cannot be cast to com.mfh.comn.net.data.RspBean
+                    String result = null;
+                    if (rspData != null) {
+                        RspValue<String> retValue = (RspValue<String>) rspData;
+                        result = retValue.getValue();
+                    }
+
+                    //出库成功:1-556637
+                    DialogUtil.showHint("抢单成功");
+                    ZLogger.d(String.format("抢单成功: %s", result));
+                    onSubmitSuccess();
+                }
+
+                @Override
+                protected void processFailure(Throwable t, String errMsg) {
+                    super.processFailure(t, errMsg);
+                    onSubmitError(errMsg);
+                }
+            }
+            , String.class
+            , MfhApplication.getAppContext()) {
+    };
+
+    /**
+     * 提交处理中
+     */
+    public void onSubmitProcess() {
+        showProgressDialog(ProgressDialog.STATUS_PROCESSING, "请稍候...", false);
+    }
+
+    /**
+     * 提交失败
+     */
+    public void onSubmitError(String errorMsg) {
+        if (!StringUtils.isEmpty(errorMsg)) {
+            showProgressDialog(ProgressDialog.STATUS_ERROR, errorMsg, true);
+            ZLogger.df(errorMsg);
+        } else {
+            hideProgressDialog();
+        }
+        btnSubmit.setEnabled(true);
+    }
+
+    public void onSubmitSuccess() {
+        showProgressDialog(ProgressDialog.STATUS_DONE, "操作成功", true);
+        btnSubmit.setEnabled(true);
+        //修改商品信息成功后，清空商品信息
+//        refresh(null);
+        getActivity().setResult(Activity.RESULT_OK);
+        getActivity().finish();
+    }
+
+}
---
+++ pda-supermarket-idata2/src/main/java/com/manfenjiayuan/pda_supermarket/ui/buyer/BuyScOrderFragment.java
@@ -0,0 +1,232 @@
+package com.manfenjiayuan.pda_supermarket.ui.buyer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bingshanguxue.pda.widget.ScanBar;
+import com.bingshanguxue.vector_uikit.slideTab.TopFragmentPagerAdapter;
+import com.bingshanguxue.vector_uikit.slideTab.TopSlidingTabStrip;
+import com.manfenjiayuan.pda_supermarket.R;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderCancelledFragment;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderServicedFragment;
+import com.manfenjiayuan.pda_supermarket.ui.common.ScOrderServicingFragment;
+import com.mfh.framework.core.utils.StringUtils;
+import com.mfh.framework.uikit.base.BaseFragment;
+import com.mfh.framework.uikit.widget.ViewPageInfo;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+
+
+/**
+ * 买手——线上订单
+ * <table>
+ *     <tr><th>订单状态</th><th>变更操作</th><th>说明</th></tr>
+ *     <tr>
+ *         <td>待购买</td>
+ *         <td>用户下单</td>
+ *         <td>用户下完订单，尚未为买手抢单</td>
+ *     </tr>
+ *     <tr>
+ *         <td>购买中</td>
+ *         <td>买手抢单</td>
+ *         <td>买手抢完单后，用户就不能取消订单了</td>
+ *     </tr>
+ *     <tr>
+ *         <td>待配送</td>
+ *         <td>1.买手点击“完成”
+ 2.pos端拣货完成</td>
+ *         <td>买手组完货后，可以走收银台进行订单的二次调整，也可以自己点击完成，组完货，买手只需将打包后的商品放在配送处即可，无需关心哪个骑手来送</td>
+ *     </tr>
+ *     <tr>
+ *         <td>配送中</td>
+ *         <td>骑手揽货</td>
+ *         <td>骑手通过PDA揽件后，订单就变成了配送中</td>
+ *     </tr>
+ *     <tr>
+ *         <td>已完成</td>
+ *         <td>骑手妥投</td>
+ *         <td>骑手已将订单交付用户，点击完成</td>
+ *     </tr>
+ *     <tr>
+ *         <td>有退货</td>
+ *         <td>骑手取消单品</td>
+ *         <td>大部分情况用户是只会退部分商品，骑手只需在订单里删除部分商品即可，如果要整单取消，也是相同的操作，只需把所有商品删除即可。有退单的订单应该也是属于“已完成”的订单，有退货应该是订单另外一个维度的属性</td>
+ *     </tr>
+ *     <tr>
+ *         <td>已取消</td>
+ *         <td>用户取消</td>
+ *         <td>用户下完单后，可以取消该订单，但如果该订单已被买手抢单了，就不能退单了，在用户的订单详情页面里就没有取消的按钮了</td>
+ *     </tr>
+ *
+ * </table>
+ * Created by Nat.ZZN(bingshanguxue) on 15/8/30.
+ */
+public class BuyScOrderFragment extends BaseFragment {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.scanBar)
+    public ScanBar mScanBar;
+    @Bind(R.id.tab_page)
+    TopSlidingTabStrip mTabStrip;
+    @Bind(R.id.viewpager_pagecontent)
+    ViewPager mViewPager;
+    private TopFragmentPagerAdapter viewPagerAdapter;
+
+    @Bind(R.id.fab_submit)
+    public FloatingActionButton btnSubmit;
+
+    public static BuyScOrderFragment newInstance(Bundle args) {
+        BuyScOrderFragment fragment = new BuyScOrderFragment();
+
+        if (args != null) {
+            fragment.setArguments(args);
+        }
+        return fragment;
+    }
+
+    @Override
+    protected boolean isResponseBackPressed() {
+        return true;
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_invfind_create;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    protected void createViewInner(View rootView, ViewGroup container, Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        if (args != null) {
+            animType = args.getInt(EXTRA_KEY_ANIM_TYPE, ANIM_TYPE_NEW_NONE);
+        }
+
+        mToolbar.setTitle("买手订单");
+        if (animType == ANIM_TYPE_NEW_FLOW) {
+            mToolbar.setNavigationIcon(R.drawable.ic_toolbar_close);
+        } else {
+            mToolbar.setNavigationIcon(R.drawable.ic_toolbar_back);
+        }
+        mToolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getActivity().onBackPressed();
+                    }
+                });
+//        // Set an OnMenuItemClickListener to handle menu item clicks
+//        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                // Handle the menu item
+//                int id = item.getItemId();
+//                if (id == R.id.action_submit) {
+//                    submit();
+//                }
+//                return true;
+//            }
+//        });
+//        // Inflate a menu to be displayed in the toolbar
+//        mToolbar.inflateMenu(R.menu.menu_inv_recv);
+
+        mScanBar.setVisibility(View.GONE);
+        initTabs();
+
+        btnSubmit.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onBackPressed() {
+//        if (goodsAdapter.getItemCount() > 0) {
+//            showConfirmDialog("退出后商品列表将会清空，确定要退出吗？",
+//                    "退出", new DialogInterface.OnClickListener() {
+//
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.dismiss();
+//
+//                            getActivity().setResult(Activity.RESULT_CANCELED);
+//                            getActivity().finish();
+//                        }
+//                    }, "点错了", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.dismiss();
+//                        }
+//                    });
+//        } else {
+        getActivity().setResult(Activity.RESULT_CANCELED);
+        getActivity().finish();
+//        }
+
+        return isResponseBackPressed();
+    }
+
+
+    private void initTabs() {
+        mTabStrip.setOnClickTabListener(null);
+        mTabStrip.setOnPagerChange(new TopSlidingTabStrip.OnPagerChangeLis() {
+            @Override
+            public void onChanged(int page) {
+//                refreshData(page);
+//                if (page != 0){
+//                    mScanBar.requestFocusEnd();
+//                }
+            }
+        });
+
+        viewPagerAdapter = new TopFragmentPagerAdapter(getChildFragmentManager(),
+                mTabStrip, mViewPager, R.layout.tabitem_text_large);
+        ArrayList<ViewPageInfo> mTabs = new ArrayList<>();
+
+        //订单已创建，买手待抢单组货
+        mTabs.add(new ViewPageInfo("待组货", "待组货", PrepareAbleOrdersFragment.class,
+                genBundle(0, null)));
+        //买手抢单后待组货
+        mTabs.add(new ViewPageInfo("组货中", "组货中", ScOrderServicingFragment.class,
+                genBundle(0, null)));
+        //买手组货完成，骑手待揽件
+        mTabs.add(new ViewPageInfo("待配送", "待配送", ScOrderServicedFragment.class,
+                genBundle(0, "3")));
+        //骑手揽件后待妥投
+        mTabs.add(new ViewPageInfo("配送中", "配送中", ScOrderServicedFragment.class,
+                genBundle(0, "6")));
+        //骑手妥投成功
+        mTabs.add(new ViewPageInfo("妥投", "妥投", ScOrderServicedFragment.class,
+                genBundle(0, "12")));
+        //有退货商品
+        mTabs.add(new ViewPageInfo("有退货", "有退货", ScOrderCancelledFragment.class,
+                genBundle(0, null)));
+
+        viewPagerAdapter.addAllTab(mTabs);
+        mViewPager.setOffscreenPageLimit(mTabs.size());
+    }
+
+    /**
+     * */
+    private Bundle genBundle(int roleType, String status){
+        Bundle args = new Bundle();
+        args.putInt(ScOrderServicedFragment.EXTRA_KEY_ROLETYPE, roleType);
+        if (!StringUtils.isEmpty(status)){
+            args.putString(ScOrderServicedFragment.EXTRA_KEY_STATUS, status);
+        }
+        return args;
+    }
+}
