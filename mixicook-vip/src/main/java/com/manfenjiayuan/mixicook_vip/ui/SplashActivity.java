c457c2f04cde2724d846ae8931ff5058925eb666
bingshanguxue <bingshanguxue@gmail.com>
2016-11-07 15:11:55 +0800

    新增 动态申请权限功能。

--- mixicook-vip/src/main/java/com/manfenjiayuan/mixicook_vip/ui/SplashActivity.java
+++ mixicook-vip/src/main/java/com/manfenjiayuan/mixicook_vip/ui/SplashActivity.java
@@ -1,11 +1,17 @@
 package com.manfenjiayuan.mixicook_vip.ui;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
 
 import com.manfenjiayuan.business.ui.SignInActivity;
@@ -13,14 +19,20 @@ import com.manfenjiayuan.mixicook_vip.AppContext;
 import com.manfenjiayuan.mixicook_vip.R;
 import com.manfenjiayuan.mixicook_vip.utils.AppHelper;
 import com.mfh.comn.upgrade.DbVersion;
+import com.mfh.framework.Constants;
 import com.mfh.framework.anlaysis.AnalysisAgent;
 import com.mfh.framework.anlaysis.AppInfo;
 import com.mfh.framework.anlaysis.logger.ZLogger;
+import com.mfh.framework.core.utils.StringUtils;
 import com.mfh.framework.helper.SharedPreferencesManager;
 import com.mfh.framework.login.logic.MfhLoginService;
+import com.mfh.framework.system.PermissionUtil;
 import com.mfh.framework.uikit.UIHelper;
 import com.mfh.framework.uikit.base.BaseActivity;
 import com.mfh.framework.uikit.base.InitActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 
@@ -69,10 +81,65 @@ public class SplashActivity extends InitActivity {
     }
 
     @Override
+    public void doAsyncTask() {
+        if (!requestPermissions()){
+            return;
+        }
+
+        super.doAsyncTask();
+        // TODO: 07/11/2016
+    }
+
+    @Override
     public void initPrimary() {
         super.initPrimary();
 
+//        申请权限
+
         DbVersion.setDomainVersion("PDASUPERMARKET.CLIENT.DB.UPGRADE", 0);
+    }
+
+    /**
+     * 权限申请
+     * */
+    private boolean requestPermissions(){
+        ArrayList<String> expectPermissions = new ArrayList<>();
+        expectPermissions.add(Manifest.permission.CAMERA);
+        expectPermissions.add(Manifest.permission.READ_CONTACTS);
+        expectPermissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        expectPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        //MicroPhone:录音
+        expectPermissions.add(Manifest.permission.RECORD_AUDIO);
+        //Phone:拨打电话
+        expectPermissions.add(Manifest.permission.CALL_PHONE);
+        //SMS:短信
+        expectPermissions.add(Manifest.permission.RECEIVE_SMS);
+        //Storage:文件存储
+        expectPermissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        expectPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+        List<String> lackPermissions = new ArrayList<>();
+        for (String permission : expectPermissions){
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ZLogger.d(getString(R.string.permission_not_granted, permission));
+                lackPermissions.add(permission);
+            }
+        }
+
+        int size = lackPermissions.size();
+        if (size > 0){
+            String[] permissions = new String[size];
+            for (int i = 0; i < size; i++){
+                permissions[i] = lackPermissions.get(i);
+            }
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this,
+                    permissions, Constants.REQUEST_CODE_PERMISSIONS);
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -130,6 +197,14 @@ public class SplashActivity extends InitActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
+            case ARCode.ARC_ANDROID_SETTINGS: {
+                ZLogger.d(StringUtils.decodeBundle(data.getExtras()));
+                if (resultCode == Activity.RESULT_OK) {
+                    doAsyncTask();
+                }
+            }
+            break;
+
             case ARCode.ARC_SIGNIN: {
                 if (resultCode == Activity.RESULT_OK) {
                     redirect2Main();
@@ -139,6 +214,38 @@ public class SplashActivity extends InitActivity {
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            // BEGIN_INCLUDE(permission_result)
+            // Received permission result for camera permission.
+            ZLogger.i("Received response for permissions request.");
+
+            // Check if the only required permission has been granted
+            boolean isGrannted = PermissionUtil.verifyPermissions(grantResults);
+            ZLogger.d("isGrannted=" + isGrannted);
+            if (isGrannted){
+                doAsyncTask();
+            }
+            else{
+//                MANAGE_APP_PERMISSIONS
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package:" + getPackageName())); // 根据包名打开对应的设置界面
+//                startActivity(intent);
+                startActivityForResult(intent, ARCode.ARC_ANDROID_SETTINGS);
+            }
+            // END_INCLUDE(permission_result)
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
 
